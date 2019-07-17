31a892dd7f7ef99fb3fcc1e9a0cc6eb2626bd65c
Frank Mazzarella <mazzarella.frank@gmail.com>
2019-07-16 21:04:03 -0400

    adding basic request modules and gitignore

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const request = require('request');
+
 console.log('EVE Online system activity monitor is running');
+
+request.get('https://esi.evetech.net/latest/universe/system_jumps/?datasource=tranquility', (error, response, body) => {
+  console.log(JSON.parse(body));
+});
+
+request.get('https://esi.evetech.net/latest/universe/systems/?datasource=tranquility', (error, response, body) => {
+  console.log(JSON.parse(body));
+});
+
+request.get('https://esi.evetech.net/latest/universe/system_kills/?datasource=tranquility', (error, response, body) => {
+  console.log(JSON.parse(body));
+});
