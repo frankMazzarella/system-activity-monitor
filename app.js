b5dfa0dd64e8bf880f59f64ec592ba6107a9a090
Frank Mazzarella <mazzarella.frank@gmail.com>
2019-07-19 13:37:18 -0400

    adding view engine

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+const express = require('express');
+const compression = require('compression');
+
 const solarSystemsService = require('./service/solar-systems.service');
 const eveApiService = require('./service/eve-api.service');
 
-// const systems = solarSystemsService.getSystems();
 // TODO: needs a logger
+
+const app = express();
+const port = '3000';
+app.use(compression());
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index', { systems: solarSystemsService.getSystems() }));
+app.get('/systems', (req, res) => res.send(solarSystemsService.getSystems()));
+app.get('/healthcheck', (req, res) => res.send({ uptime: process.uptime() }));
+app.listen(port, () => console.log('express is running'));
 
 eveApiService.getSystemJumps()
   .then(systemJumps => solarSystemsService.updateSystemJumps(systemJumps))
